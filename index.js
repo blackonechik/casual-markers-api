965452b5abbf9a8b109c91259f65e5c72e231d8e
BlackOne <surnin.vladislav@gmail.com>
2024-12-15 21:00:56 +0300

    f

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const gData = [...Array(N).keys()].map(() => ({
   lat: (Math.random() - 0.5) * 180,
   lng: (Math.random() - 0.5) * 360,
   size: 7 + Math.random() * 30,
-  color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+  color: "red",
   title: `Title ${Math.round(Math.random() * 100)}`,
   description: `Description ${Math.round(Math.random() * 100)}`,
   url: `https://example.com/${Math.round(Math.random() * 100)}`,
