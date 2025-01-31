77fdaeb1a4ab516e08d0c8a76bbfc9308642f81c
BlackOne <surnin.vladislav@gmail.com>
2025-01-31 19:48:40 +0300

    g

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ const camera = new THREE.PerspectiveCamera();
 camera.aspect = container.clientWidth / container.clientHeight;
 camera.updateProjectionMatrix();
 camera.position.z = 290;
+camera.position.set(32, 32, 290);
 
 const tbControls = new TrackballControls(camera, renderers[0].domElement);
 tbControls.noPan = true;
